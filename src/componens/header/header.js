e1d7a343b8d5adad6f2667372a5eac8ee6f504ee
Sling-J <nurel32@gmail.com>
2019-11-22 12:43:23 +0600

    add (page loader)

--- src/componens/header/header.js
+++ src/componens/header/header.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import style from './header.module.sass';
 import Logo from '../../images/chatlead.png';
 import {Link} from 'react-router-dom';
 import Menu from '../../images/menu.png';
 
 const Header = (props) => {
-
-    return (
-        <div className={style.mainContainer}>
-            <img src={Logo} alt={'logo'} />
+   return (
+      <Fragment>
+         <div className={style.mainContainer}>
+            <img src={Logo} alt={'logo'}/>
             <Link to={'/bots'} className={style.link}>Боты</Link>
-            <img src={Menu} alt={'menu'} />
-        </div>
-    )
+            <img src={Menu} alt={'menu'}/>
+         </div>
+      </Fragment>
+   )
 };
 
 export default Header;
