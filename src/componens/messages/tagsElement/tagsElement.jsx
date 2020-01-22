dd70fe2925ddec369ac0cb3f81a939c8fa8f7890
Nurel <nurel32@gmail.com>
2020-01-22 19:51:06 +0600

    add (select add)

--- src/componens/messages/tagsElement/tagsElement.jsx
+++ src/componens/messages/tagsElement/tagsElement.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import HoverBarForMessage from '../hoverBarForMessage/hoverBarForMessage';
-import {Select} from "antd";
+import {Select, Divider, Icon} from "antd";
 
 import style from "./tagsElement.module.scss";
 
-// const { Option } = Select;
+const { Option } = Select;
 
 const TagsElement = props => {
-   // const children = [];
-   //
-   // for (let i = 10; i < 20; i++) {
-   // 	children.push(<Option key={i.toString(20) + i}>{i.toString(20) + i}</Option>);
-   // }
+   const children = [];
+
+   const [searchValue, setSearchValue] = useState('');
+
+   for (let i = 10; i < 20; i++) {
+   	children.push(<Option key={i.toString(20) + i}>{i.toString(20) + i}</Option>);
+   }
 
    function handleChange(value) {
       console.log(`selected ${value}`);
@@ -24,7 +26,10 @@ const TagsElement = props => {
             <HoverBarForMessage {...props}/>
          </div>
 
-         <p className={style.tagsElementContainerTitle}>Используйте этот блок, чтобы добавить или убрать теги в работе цепочки</p>
+         <p className={style.tagsElementContainerTitle}>
+            Используйте этот блок, чтобы добавить
+            или убрать теги в работе цепочки
+         </p>
 
          <div className={style.tagsElementContainerField}>
             <Select
@@ -32,7 +37,26 @@ const TagsElement = props => {
                style={{width: '100%'}}
                placeholder="Добавить теги"
                onChange={handleChange}
-            />
+               onSearch={value => setSearchValue(value)}
+               dropdownRender={menu => {
+                  const result = children.find(item => item.key === searchValue);
+
+                  return (
+                     <div>
+                        <div
+                           style={{padding: '4px 8px', cursor: 'pointer'}}
+                           onMouseDown={e => e.preventDefault()}
+                        >
+                           {!result && children.find(item => item.key !== searchValue) && searchValue.length !== 0 && (<><Icon type="plus"/> Создать «{searchValue}»</>)}
+                        </div>
+                        <Divider style={{margin: '4px 0'}}/>
+                        {menu}
+                     </div>
+                  )
+               }}
+            >
+               {children}
+            </Select>
          </div>
 
          <div className={style.tagsElementContainerField}>
